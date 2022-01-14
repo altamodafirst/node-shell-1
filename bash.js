36929555f0ee625f9b1405e9bbf2e38b9cdc4ffa
Lauren Noble <lauren.noble3@gmail.com>
2022-01-14 15:04:48 -0500

    nodemon

---
+++ bash.js
@@ -0,0 +1,9 @@
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+    const cmd = process.cwd();
+
+    process.stdout.write(cmd);
+    process.stdout.write('\nprompt > ');
+});
